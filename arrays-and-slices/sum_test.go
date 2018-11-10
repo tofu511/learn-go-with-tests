ee9518412f3ad43b6d9aed29642efa032465ab47
tofu511 <mokmok511@gmail.com>
2018-11-11 00:11:19 +0900

    refactor

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -32,18 +32,27 @@ func TestSum(t *testing.T)  {
 func TestSumAll(t *testing.T)  {
 	got := SumAll([]int{1,2}, []int{0,9})
 	want := []int{3,9}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	checkSums(t, got, want)
 }
 
 func TestSumAllTails(t *testing.T)  {
-	got := SumAllTails([]int{1,2,5}, []int{0,9,7})
-	want := []int{7,16}
 
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1,2,5}, []int{0,9,7})
+		want := []int{7,16}
+		checkSums(t ,got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3,4,5})
+		want := []int{0,9}
+		checkSums(t, got, want)
+	})
+}
+
+func checkSums(t *testing.T, got, want []int)  {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
-
 }
