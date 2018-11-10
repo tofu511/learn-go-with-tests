e1aa64b0ee2bf17f5b2846fc7e4bc73f98f680b8
tofu511 <mokmok511@gmail.com>
2018-11-10 21:01:17 +0900

    refactor

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,1 +1,8 @@
 package iteration
+
+func Repeat(character string, repeatCount int) (repeated string) {
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,1 +1,48 @@
 package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T)  {
+
+	t.Run("Repeat('a', 5) should return 'aaaaa' ", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, repeated)
+		}
+	})
+
+	t.Run("Repeat('f', 5) should return 'fffff' ", func(t *testing.T) {
+		repeated := Repeat("f", 5)
+		expected := "fffff"
+
+		if repeated != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, repeated)
+		}
+	})
+
+	t.Run("Repeat('a', 3) should return 'aaa'", func(t *testing.T) {
+		repeated := Repeat("a", 3)
+		expected := "aaa"
+
+		if repeated != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, repeated)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("d", 10)
+	fmt.Println(repeated)
+	// Output: dddddddddd
+}
