6ba3ee6862657b7c3e9fe2e0401941fa26cd7b8f
tofu511 <mokmok511@gmail.com>
2018-11-11 23:55:45 +0900

    refactor

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,14 +1,12 @@
 package structs_methods_interfaces
 
 import (
-	"fmt"
 	"math"
 )
 
 type Shape interface {
 	Area() float64
-	String() string
-} 
+}
 
 type Rectangle struct {
 	Width float64
@@ -19,20 +17,12 @@ func (r Rectangle) Area() float64  {
 	return r.Width  * r.Height
 }
 
-func (r Rectangle) String() string {
-	return fmt.Sprintf("Rectangle: %.2f, %.2f", r.Height, r.Width)
-}
-
 type Circle struct {
 	Radius float64
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
-}
-
-func (c Circle) String() string {
-	return fmt.Sprintf("Circle: %.2f", c.Radius)
 }
 
 type Triangle struct {
@@ -42,10 +32,6 @@ type Triangle struct {
 
 func (t Triangle) Area() float64 {
 	return (t.Base * t.Height) / 2
-}
-
-func (t Triangle) String() string {
-	return fmt.Sprintf("Triangle: %.2f, %.2f", t.Base, t.Height)
 }
 
 func Perimeter(rectangle Rectangle) float64  {
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -10,26 +10,27 @@ func TestPerimeter(t *testing.T)  {
 	want := 40.0
 
 	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+		t.Errorf("got %.2f hasArea %.2f", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{12, 6}, want: 72.0},
-		{shape: Circle{10}, want: 314.1592653589793},
-		{shape: Triangle{12, 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		t.Run(tt.shape.String(), func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 			got := tt.shape.Area()
-			if got != tt.want {
-				t.Errorf("got %.2f want %.2f", got, tt.want)
+			if got != tt.hasArea {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
 			}
 		})
 	}
