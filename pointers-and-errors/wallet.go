dabfe36f0701d56261ef503b7636d4a0dbe689db
tofu511 <mokmok511@gmail.com>
2018-11-12 21:53:01 +0900

    add bitcoin

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -1,13 +1,25 @@
 package pointers_and_errors
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int  {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
