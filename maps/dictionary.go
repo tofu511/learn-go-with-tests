eda38a097e210436f7f57b33475eace56beafd90
tofu511 <mokmok511@gmail.com>
2018-11-14 00:24:27 +0900

    added existing word test case

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,8 +4,10 @@ import "github.com/pkg/errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word were looking for")
-
+var (
+	ErrNotFound = errors.New("could not find the word were looking for")
+	ErrWordExisting = errors.New("cannot add word because it already exists")
+)
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
@@ -15,6 +17,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(key, value string)  {
-	d[key] = value
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExisting
+	default:
+		return err
+	}
+
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -17,16 +17,27 @@ func TestSearch(t *testing.T)  {
 
 		assertError(t, err, ErrNotFound)
 	})
-
 }
 
 func TestAdd(t *testing.T)  {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExisting)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string)  {
@@ -54,5 +65,4 @@ func assertDefinition(t *testing.T, dictionary Dictionary, word, definition stri
 	if got != definition {
 		t.Errorf("got '%s' want '%s'", got, definition)
 	}
-
 }
