a33881e89eab5eaeb66cabbe713281ab6a24b696
tofu511 <mokmok511@gmail.com>
2018-11-15 00:40:15 +0900

    change writer type

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -1,10 +1,19 @@
 package dependency_injection
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
+
+//func MyGreetHandler(w http.ResponseWriter, r *http.Request)  {
+//	Greet(w, "world")
+//}
+//
+//func main() {
+//	Greet(os.Stdout, "world")
+//	http.ListenAndServe(":5000", http.HandlerFunc(MyGreetHandler))
+//}
