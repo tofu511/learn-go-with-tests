e6138ca67d95285ca69db4b071320881ed64580d
tofu511 <mokmok511@gmail.com>
2018-11-15 22:47:30 +0900

    implements countdown

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(writer io.Writer)  {
-	fmt.Fprint(writer, "3")
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 + time.Second)
+		fmt.Fprintln(writer, i)
+	}
+	time.Sleep(1 + time.Second)
+	fmt.Fprint(writer, finalWord)
 }
 
 func main()  {
