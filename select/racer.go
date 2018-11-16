d5e120c5f7010876406cf9527791703c3844e77e
tofu511 <mokmok511@gmail.com>
2018-11-16 13:06:30 +0900

    fix typo

--- select/racer.go
+++ select/racer.go
@@ -6,8 +6,8 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResposeTime(a)
-	bDuration := measureResposeTime(b)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
@@ -16,7 +16,7 @@ func Racer(a, b string) (winner string) {
 	return b
 }
 
-func measureResposeTime(url string) time.Duration {
+func measureResponseTime(url string) time.Duration {
 	start := time.Now()
 	http.Get(url)
 	return time.Since(start)
