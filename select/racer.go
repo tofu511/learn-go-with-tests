9792ab6fcf971fb70c7a1b29799a9315fbd36764
tofu511 <mokmok511@gmail.com>
2018-11-16 14:12:40 +0900

    use select

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,33 @@
 package _select
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+var tenSecondTimeout = 10 * time.Second
 
-	if aDuration < bDuration {
-		return a
-	}
-
-	return b
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+		case <- ping(a):
+			return a, nil
+		case <- ping(b):
+			return b, nil
+		case <- time.After(timeout):
+			return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
