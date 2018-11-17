037e3cef2ee648546c3a3ea777d48ab682ec9903
tofu511 <mokmok511@gmail.com>
2018-11-17 13:41:07 +0900

    add http post test

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -64,6 +64,22 @@ func TestGETPlayer(t *testing.T)  {
 	})
 }
 
+func TestStoreWins(t *testing.T)  {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
+
 func newGetScoreRequest(name string) *http.Request {
 	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
 	return request
