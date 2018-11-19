e6be32a971db570cc82ff064a8731d8ec090067b
tofu511 <mokmok511@gmail.com>
2018-11-19 23:51:07 +0900

    split files

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -4,10 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"reflect"
 	"testing"
 )
@@ -188,79 +186,6 @@ func TestLeague(t *testing.T)  {
 	})
 }
 
-func TestFileSystemStore(t *testing.T)  {
-
-	t.Run("/league from a reader", func(t *testing.T) {
-		database, cleanDatabase := createTempFile(t, `[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
-		defer cleanDatabase()
-
-		store := FileSystemPlayerStore{database}
-
-		got := store.GetLeague()
-
-		want := []Player{
-			{"Cleo", 10},
-			{"Chris", 33},
-		}
-
-		assertLeague(t, got, want)
-
-		got = store.GetLeague()
-		assertLeague(t, got, want)
-	})
-
-	t.Run("get player score", func(t *testing.T) {
-		database, cleanDatabase := createTempFile(t, `[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
-		defer cleanDatabase()
-
-		store := FileSystemPlayerStore{database}
-
-		got := store.GetPlayerScore("Chris")
-		want := 33
-		assertScoreEqual(t, got, want)
-	})
-
-	t.Run("store wins for existing players", func(t *testing.T) {
-		database, cleanDatabase := createTempFile(t, `[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
-		defer cleanDatabase()
-
-		store := FileSystemPlayerStore{database}
-
-		store.RecordWin("Chris")
-
-		got := store.GetPlayerScore("Chris")
-		want := 34
-		assertScoreEqual(t, got, want)
-	})
-
-	t.Run("store wins for new players", func(t *testing.T) {
-		database, cleanDatabase := createTempFile(t, `[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
-		defer cleanDatabase()
-
-		store := FileSystemPlayerStore{database}
-
-		store.RecordWin("Pepper")
-
-		got := store.GetPlayerScore("Pepper")
-		want := 1
-		assertScoreEqual(t, got, want)
-	})
-}
-
-func assertScoreEqual(t *testing.T, got int, want int) {
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-
 const jsonContentType = "application/json"
 
 func assertContentType(t *testing.T, response *httptest.ResponseRecorder, want string) {
@@ -315,22 +240,4 @@ func assertStatus(t *testing.T, got, want int)  {
 	if got != want {
 			t.Errorf("got status %d want %d", got, want)
 	}
-}
-
-func createTempFile(t *testing.T, initialData string) (io.ReadWriteSeeker, func()) {
-	t.Helper()
-
-	tmpFile, err := ioutil.TempFile("", "db")
-
-	if err != nil {
-		t.Fatalf("could not create temp file %v", err)
-	}
-
-	tmpFile.Write([]byte(initialData))
-
-	removeFile := func() {
-		os.Remove(tmpFile.Name())
-	}
-
-	return tmpFile, removeFile
 }
